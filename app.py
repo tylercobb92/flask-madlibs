06b61cea64d7292ae3b3efbacb20fc90ccfa7857
tyler <tylercobb92@gmail.com>
2022-01-13 17:35:09 -0500

    route / provides a template for madlib, route /story returns the story with placeholders

---
+++ app.py
@@ -0,0 +1,21 @@
+from distutils.log import debug
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "sneaky"
+
+debug = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def madlib_questions():
+    prompts = story.prompts
+    return render_template('prompts.html', prompts=prompts)
+
+
+@app.route('/story')
+def write_story():
+    template = story.generate(request.args)
+    return template
